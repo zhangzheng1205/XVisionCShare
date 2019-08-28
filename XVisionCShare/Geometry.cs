17064c4fc093e125ed8a63e8cf9a53e8fd88cd49
陈晓聪 <a1194457876@outlook.com>
2019-08-28 16:11:32 +0800

    添加项目文件。

---
+++ XVisionCShare/Geometry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XVisionCShare.Geometry
+{
+    public struct Point
+    {
+        public int x;
+        public int y;
+    }
+
+    public struct PointFloat
+    {
+        public float x;
+        public float y;
+    }
+
+    public struct PointDouble
+    {
+        public double x;
+        public double y;
+    }
+
+}
